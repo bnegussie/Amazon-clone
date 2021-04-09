024a98a89dd825e3adc56c5054b30f679cd8e3b6
bnegussie <bnegussie3@protonmail.com>
2021-04-09 11:10:39 -0700

    setup the display-logic and layout for removing a single item and removing multiple items from the shopping cart.

--- client/src/components/checkout/CheckoutProduct.js
+++ client/src/components/checkout/CheckoutProduct.js
@@ -1,56 +1,60 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useStateValue } from '../data-manager/StateProvider';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useStateValue } from "../data-manager/StateProvider";
 
-import "./../../App.css"
+import "./../../App.css";
 
 function CheckoutProduct({ id, shortTitle, title, price, image, quantity }) {
+	// eslint-disable-next-line
+	const [{ cart }, dispatch] = useStateValue();
 
-    // eslint-disable-next-line
-    const [{ cart }, dispatch] = useStateValue();
+	function removeFromCart() {
+		dispatch({
+			type: "REMOVE_FROM_CART",
+			item: {
+				id: id,
+			},
+		});
+	}
 
-    function removeFromCart() {
-        dispatch({
-            type: "REMOVE_FROM_CART",
-            item: {
-                id: id
-            }
-        });
-    }
+	return (
+		<div className="checkout-product-container">
+			<img
+				className="checkout-product-img"
+				src={image}
+				alt={shortTitle ? shortTitle : title}
+			/>
 
+			<div className="checkout-product-info-container">
+				<p className="checkout-product-title">{title}</p>
 
+				<p className="checkout-product-price">
+					<small>$</small>
+					<strong>{price}</strong>
+				</p>
 
-    return (
-        <div className="checkout-product-container">
-            <img 
-                className="checkout-product-img"
-                src={image}
-                alt={ shortTitle ? shortTitle : title }
-            />
+				<div className="quantity-and-remove-container">
+					<p className="quantity-value">Quantity: {quantity}</p>
 
-            <div className="checkout-product-info-container">
-                <p className="checkout-product-title">{ title }</p>
+					{quantity > 1 ? (
+						<div>
+							<Link className="remove-all-items-link" to="#" onClick={removeFromCart}>
+								Remove all
+							</Link>
 
-                <p className="checkout-product-price">
-                    <small>$</small>
-                    <strong>{ price }</strong>
-                </p>
-
-                <div>
-                    <h3>Quantity: { quantity }</h3>
-                    <Link 
-                        className="remove-item-link" 
-                        to="#"
-                        onClick={removeFromCart} >
-                        Delete
-                    </Link>
-                </div>
-
-
-            </div>
-
-        </div>
-    )
+							<Link className="remove-one-item-link" to="#" onClick={removeFromCart}>
+								Remove one item
+							</Link>
+						</div>
+					) : (
+						<Link className="remove-item-link" to="#" onClick={removeFromCart}>
+							Remove item
+						</Link>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default CheckoutProduct;
