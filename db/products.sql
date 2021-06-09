0b80d8b8046f43f9238c1f9ff98797889c54162c
bnegussie <bnegussie3@protonmail.com>
2021-06-09 14:24:06 -0700

    designed and implemented the database table for Products.

--- db/products.sql
+++ db/products.sql
@@ -2,109 +2,27 @@ CREATE DATABASE amazon_clone;
 
 CREATE TABLE product_categories(
     c_name VARCHAR(255) PRIMARY KEY NOT NULL UNIQUE,
-    table_name VARCHAR(255) NOT NULL
+    c_name_in_db VARCHAR(255) NOT NULL
 );
 
 COPY product_categories FROM 'C:\Users\Brook\Desktop\product_categories.csv' WITH CSV HEADER;
 
 
-
-CREATE TABLE c_arts_crafts_and_sewing(
-    id SERIAL PRIMARY KEY NOT NULL UNIQUE,
+CREATE TABLE products(
+    p_id SERIAL PRIMARY KEY NOT NULL UNIQUE,
+    seller_id UUID NOT NULL,
+    seller_name VARCHAR(255) NOT NULL,
     p_name VARCHAR(255) NOT NULL,
-    img_1_path VARCHAR(255) NOT NULL,
-    img_2_path VARCHAR(255),
-    img_3_path VARCHAR(255),
-    img_4_path VARCHAR(255),
-    img_5_path VARCHAR(255)
-);
-
-CREATE TABLE c_automotive_parts_and_accessories(
-    id SERIAL PRIMARY KEY NOT NULL UNIQUE,
-    p_name VARCHAR(255) NOT NULL,
-    img_1_path VARCHAR(255) NOT NULL,
-    img_2_path VARCHAR(255),
-    img_3_path VARCHAR(255),
-    img_4_path VARCHAR(255),
-    img_5_path VARCHAR(255)
-);
-
-CREATE TABLE c_books(
-    id SERIAL PRIMARY KEY NOT NULL UNIQUE,
-    p_name VARCHAR(255) NOT NULL,
-    img_1_path VARCHAR(255) NOT NULL,
-    img_2_path VARCHAR(255),
-    img_3_path VARCHAR(255),
-    img_4_path VARCHAR(255),
-    img_5_path VARCHAR(255)
-);
-
-CREATE TABLE c_cds_and_vinyl(
-    id SERIAL PRIMARY KEY NOT NULL UNIQUE,
-    p_name VARCHAR(255) NOT NULL,
-    img_1_path VARCHAR(255) NOT NULL,
-    img_2_path VARCHAR(255),
-    img_3_path VARCHAR(255),
-    img_4_path VARCHAR(255),
-    img_5_path VARCHAR(255)
-);
-
-CREATE TABLE c_digital_music(
-    id SERIAL PRIMARY KEY NOT NULL UNIQUE,
-    p_name VARCHAR(255) NOT NULL,
-    img_1_path VARCHAR(255) NOT NULL,
-    img_2_path VARCHAR(255),
-    img_3_path VARCHAR(255),
-    img_4_path VARCHAR(255),
-    img_5_path VARCHAR(255)
-);
-
-CREATE TABLE c_electronics(
-    id SERIAL PRIMARY KEY NOT NULL UNIQUE,
-    p_name VARCHAR(255) NOT NULL,
-    img_1_path VARCHAR(255) NOT NULL,
-    img_2_path VARCHAR(255),
-    img_3_path VARCHAR(255),
-    img_4_path VARCHAR(255),
-    img_5_path VARCHAR(255)
-);
-
-CREATE TABLE c_handmade(
-    id SERIAL PRIMARY KEY NOT NULL UNIQUE,
-    p_name VARCHAR(255) NOT NULL,
-    img_1_path VARCHAR(255) NOT NULL,
-    img_2_path VARCHAR(255),
-    img_3_path VARCHAR(255),
-    img_4_path VARCHAR(255),
-    img_5_path VARCHAR(255)
-);
-
-CREATE TABLE c_home_and_kitchen(
-    id SERIAL PRIMARY KEY NOT NULL UNIQUE,
-    p_name VARCHAR(255) NOT NULL,
-    img_1_path VARCHAR(255) NOT NULL,
-    img_2_path VARCHAR(255),
-    img_3_path VARCHAR(255),
-    img_4_path VARCHAR(255),
-    img_5_path VARCHAR(255)
-);
-
-CREATE TABLE c_industrial_and_science(
-    id SERIAL PRIMARY KEY NOT NULL UNIQUE,
-    p_name VARCHAR(255) NOT NULL,
-    img_1_path VARCHAR(255) NOT NULL,
-    img_2_path VARCHAR(255),
-    img_3_path VARCHAR(255),
-    img_4_path VARCHAR(255),
-    img_5_path VARCHAR(255)
-);
-
-CREATE TABLE c_tools_and_home_improvement(
-    id SERIAL PRIMARY KEY NOT NULL UNIQUE,
-    p_name VARCHAR(255) NOT NULL,
-    img_1_path VARCHAR(255) NOT NULL,
-    img_2_path VARCHAR(255),
-    img_3_path VARCHAR(255),
-    img_4_path VARCHAR(255),
-    img_5_path VARCHAR(255)
+    p_desc VARCHAR(255),
+    p_price money NOT NULL,
+    p_price_currency_symbol VARCHAR(10) NOT NULL,
+    p_category VARCHAR(255) NOT NULL,
+    p_available_quantities BIGINT NOT NULL,
+    p_photo_1_path VARCHAR(255) NOT NULL,
+    p_photo_2_path VARCHAR(255),
+    p_photo_3_path VARCHAR(255),
+    p_photo_4_path VARCHAR(255),
+    p_photo_5_path VARCHAR(255),
+    FOREIGN KEY (seller_id) REFERENCES users(user_id),
+    FOREIGN KEY (p_category) REFERENCES product_categories(c_name)
 );
--- db/users.sql
+++ db/users.sql
@@ -2,11 +2,12 @@
 create extension if not exists "uuid-ossp";
 
 CREATE TABLE users(
-    user_id UUID DEFAULT uuid_generate_v4(),
+    user_id UUID DEFAULT uuid_generate_v4() UNIQUE,
     user_name VARCHAR(255) NOT NULL,
     user_email VARCHAR(255) NOT NULL,
     user_pwd VARCHAR(255) NOT NULL,
     user_access_token VARCHAR(255),
     user_refresh_token VARCHAR(255),
+    seller_name VARCHAR(255),
     PRIMARY KEY (user_id)
 );
